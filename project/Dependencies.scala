408267a2c4b9b08fb4c121c03489f1a97253b196
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-01-11 11:11:14 +0000

    PLATUI-2700: Update project dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,18 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
-  val test = Seq(
-    "com.typesafe"         % "config"            % "1.4.3"    % Test,
-    "com.vladsch.flexmark" % "flexmark-all"      % "0.64.8"   % Test,
-    "org.scalatest"       %% "scalatest"         % "3.2.17"   % Test,
-    "org.scalatestplus"   %% "selenium-4-2"      % "3.2.13.0" % Test,
-    "io.cucumber"         %% "cucumber-scala"    % "8.17.0"   % Test,
-    "io.cucumber"          % "cucumber-junit"    % "7.14.0"   % Test,
-    "junit"                % "junit"             % "4.13.2"   % Test,
-    "com.novocode"         % "junit-interface"   % "0.11"     % Test,
-    "uk.gov.hmrc"         %% "webdriver-factory" % "0.46.0"   % Test
+  val test: Seq[ModuleID] = Seq(
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.62.2"   % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.17"   % Test,
+    "org.slf4j"            % "slf4j-simple"    % "1.7.36"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.15.0"   % Test,
+    "com.typesafe"         % "config"          % "1.4.3"    % Test,
+    "org.scalatestplus"   %% "selenium-4-12"   % "3.2.17.0" % Test,
+    "io.cucumber"         %% "cucumber-scala"  % "8.17.0"   % Test,
+    "io.cucumber"          % "cucumber-junit"  % "7.14.0"   % Test,
+    "junit"                % "junit"           % "4.13.2"   % Test,
+    "com.novocode"         % "junit-interface" % "0.11"     % Test
   )
 
 }
