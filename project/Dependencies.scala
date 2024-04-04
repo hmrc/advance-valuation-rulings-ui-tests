17260d403f26b0ac1bf8878bbd7ef65cbfd27385
Michael Yau <47248104+mywyau@users.noreply.github.com>
2024-04-04 15:55:59 +0100

    DDCE-5073 bump plugin and edit build.sbt (#65)
    * DDCE-5073 bump plugin
    
    * update build.sbt
    
    * revert build.sbt
    
    * bump deps
    
    * bump plugins
    
    * add () to webDriverWait

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,9 @@ object Dependencies {
 
   val test: Seq[ModuleID]    = Seq(
     "com.github.sbt" % "junit-interface" % "0.13.3",
-    "io.cucumber"    % "cucumber-junit"  % "7.15.0",
-    "io.cucumber"   %% "cucumber-scala"  % "8.20.0",
-    "uk.gov.hmrc"   %% "ui-test-runner"  % "0.21.0"
+    "io.cucumber"    % "cucumber-junit"  % "7.16.1",
+    "io.cucumber"   %% "cucumber-scala"  % "8.21.1",
+    "uk.gov.hmrc"   %% "ui-test-runner"  % "0.25.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = test
