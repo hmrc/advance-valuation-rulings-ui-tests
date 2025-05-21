497a80b66003c8ac5cdac4cefb1b9c6a3a815f57
gastonracui <gastonracui@gmail.com>
2025-05-21 09:40:08 +0100

    DDCE-6604 Standardise repository.yaml (#75)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "com.github.sbt" % "junit-interface" % "0.13.3",
-    "io.cucumber"    % "cucumber-junit"  % "7.21.1",
-    "io.cucumber"   %% "cucumber-scala"  % "8.26.1",
+    "io.cucumber"    % "cucumber-junit"  % "7.22.2",
+    "io.cucumber"   %% "cucumber-scala"  % "8.27.3",
     "uk.gov.hmrc"   %% "ui-test-runner"  % "0.45.0"
   ).map(_ % Test)
 
