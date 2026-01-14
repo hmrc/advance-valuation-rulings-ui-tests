5016f337b12809ce09d3bab03a5a7f42f9f3050d
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-01-14 13:52:54 +0000

    DDCE-7959 Upgraded version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.github.sbt" % "junit-interface" % "0.13.3",
     "io.cucumber"    % "cucumber-junit"  % "7.22.2",
     "io.cucumber"   %% "cucumber-scala"  % "8.27.3",
-    "uk.gov.hmrc"   %% "ui-test-runner"  % "0.49.0"
+    "uk.gov.hmrc"   %% "ui-test-runner"  % "0.52.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = test
