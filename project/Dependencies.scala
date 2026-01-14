0df4c8dc7144982e92c88ff21be553659fe92d55
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-01-14 15:06:44 +0000

    DDCE-7959 Dependency upgrade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   private val test: Seq[ModuleID] = Seq(
     "com.github.sbt" % "junit-interface" % "0.13.3",
-    "io.cucumber"    % "cucumber-junit"  % "7.22.2",
-    "io.cucumber"   %% "cucumber-scala"  % "8.27.3",
+    "io.cucumber"    % "cucumber-junit"  % "7.33.0",
+    "io.cucumber"   %% "cucumber-scala"  % "8.38.0",
     "uk.gov.hmrc"   %% "ui-test-runner"  % "0.52.0"
   ).map(_ % Test)
 
